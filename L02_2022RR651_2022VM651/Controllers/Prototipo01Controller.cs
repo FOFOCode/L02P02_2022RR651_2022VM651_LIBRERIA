d63ae64fcc5c5c1755640c294f47dcc029966d3b
Rodolfo Rivas Rodriguez <rodolfo.rivas@catolica.edu.sv>
2025-04-01 17:42:20 -0600

    commit fofo

---
+++ L02_2022RR651_2022VM651/Controllers/Prototipo01Controller.cs
@@ -0,0 +1,15 @@
+﻿using L02_2022RR651_2022VM651.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L02_2022RR651_2022VM651.Controllers
+{
+    public class Prototipo01Controller : Controller
+    {
+        public readonly libreriaDbContext libreriaDbContext;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
